eac651121569c5438142c7aa943e4d3af76e39ff
Musa Uçar <92794324+musaucar@users.noreply.github.com>
2022-11-19 21:26:32 +0300

    todo list update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
-function App() {
+import React, {useState} from "react";
+import { useTodoLayerValue } from "./context/TodoContext";
+import Todolist from "./components/TodoList"
+
+const App = () => {
+  const [{ todos }, dispatch] = useTodoLayerValue();
+  const [content, setContent] = useState("");
+
+  const handleSubmit= (event) => {
+    event.preventDefault();
+  };
   return (
-    <div className="App">
-      
+    <div className="container">
+      <form onSubmit={handleSubmit} className="todo-form"> 
+        <input type="text" className="todo-input" onChange={(event) => setContent(event.target.value)} value={content}/>
+        <button className="todo-button">
+          Ekle  
+        </button>
+      </form>
+
+      {/* Todo Listesi */}
+
+      <Todolist todos = {todos}/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
